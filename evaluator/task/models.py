f067bb86a745fdfe9ec1b3313eaae2a7aeaa26a2
adewolejosh <adewole.josh@gmail.com>
2020-03-27 23:39:40 +0100

    hey added year to models, haven't really worked on it yet but will, very soon. This should be my last commit here before forking the repo.

--- evaluator/task/models.py
+++ evaluator/task/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+month = [
+    ('Jan', 'January'),
+    ('Feb', 'February'),
+    ('Mar', 'March'),
+    ('Apr', 'April'),
+    ('May', 'May'),
+    ('Jun', 'June'),
+    ('Jul', 'July'),
+    ('Aug', 'August'),
+    ('Sep', 'September'),
+    ('Oct', 'October'),
+    ('Nov', 'November'),
+    ('Dec', 'December'),
+]
+
+
+class Companie(models.Model):
+    name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+
+class Information(models.Model):
+    name = models.ForeignKey(Companie, on_delete=models.CASCADE)
+    year = models.IntegerField(default=2019)
+    month = models.TextField(max_length=3, choices=month)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.month
--- evaluator/task/views.py
+++ evaluator/task/views.py
@@ -1,10 +1,45 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+from .forms import CompanyForm, InformationForm
+from .models import Information
 
 
-class InputView(TemplateView):
+class InputView(CreateView):
     template_name = 'input.html'
+    form_class = CompanyForm
+    success_url = reverse_lazy('info')
 
 
-class ResultView(TemplateView):
+class InfoView(CreateView):
+    template_name = 'info.html'
+    form_class = InformationForm
+    success_url = reverse_lazy('result')
+
+
+class ResultView(ListView):
     template_name = 'result.html'
+    model = Information
+    context_object_name = 'Information'
+
+    # def get(self, request, *args, **kwargs):
+    #     if self.model.score >= :
+    #         color = 'red'
+    #     else:
+    #         color = 'green'
+    #
+    #     return color
+
+# def result(request):
+#     template_name = 'result.html'
+#     information = Information
+#
+#     color = ''
+#     if request.method == 'GET':
+#         if information.score == 70:
+#             color = 'red'
+#         else:
+#             color = 'green'
+#         return color
+#
+#     return render(request, template_name)
